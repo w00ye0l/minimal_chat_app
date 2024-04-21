ccd86b657a973a460e9d1b93272ec97ceb27c00f
wooyeol <dnduf158@gmail.com>
2024-04-21 23:20:36 +0900

    add: connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:chat_app/auth/login_or_register.dart';
-import 'package:chat_app/pages/login_page.dart';
-import 'package:chat_app/pages/register_page.dart';
+import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/themes/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
